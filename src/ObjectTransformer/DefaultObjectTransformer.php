a825ee60dd8cd771490612e4c52a3721d7ad6aa1
David Höck <d.hoeck@alpin11.at>
2021-04-15 16:01:29 +0200

    manufacturer is optional

--- src/ObjectTransformer/DefaultObjectTransformer.php
+++ src/ObjectTransformer/DefaultObjectTransformer.php
@@ -5,6 +5,7 @@ namespace CoreShop\Bundle\GoogleShoppingBundle\ObjectTransformer;
 
 
 use CoreShop\Bundle\GoogleShoppingBundle\Model\GoogleShoppingProductInterface;
+use CoreShop\Component\Product\Model\ManufacturerInterface;
 use Vitalybaev\GoogleMerchant\Product;
 
 class DefaultObjectTransformer implements ObjectTransformerInterface
@@ -17,10 +18,13 @@ class DefaultObjectTransformer implements ObjectTransformerInterface
         $item->setId($product->getId());
         $item->setTitle($product->getName($options['locale']));
         $item->setDescription(strip_tags($product->getShortDescription($options['locale'])));
-        $item->setBrand($product->getManufacturer()->getName());
         $item->setGtin($product->getEan());
         $item->setMpn($product->getEan());
         $item->setCondition($product->getCondition());
+
+        if ($product->getManufacturer() instanceof ManufacturerInterface) {
+            $item->setBrand($product->getManufacturer()->getName());
+        }
 
         return $item;
     }
