9ce0985c3ef11cf0d42a34d84cb8183f7bf0fb6b
David Höck <d.hoeck@alpin11.at>
2021-04-15 15:15:37 +0200

    refactor composite filter

--- src/Command/GoogleShoppingCommand.php
+++ src/Command/GoogleShoppingCommand.php
@@ -7,11 +7,13 @@ namespace CoreShop\Bundle\GoogleShoppingBundle\Command;
 use CoreShop\Bundle\GoogleShoppingBundle\DataCollector\DataCollectorInterface;
 use CoreShop\Bundle\GoogleShoppingBundle\Distributor\DistributorInterface;
 use CoreShop\Bundle\GoogleShoppingBundle\ObjectTransformer\ObjectTransformerInterface;
+use CoreShop\Component\Store\Repository\StoreRepositoryInterface;
 use Pimcore\Console\AbstractCommand;
 use Pimcore\Tool;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Vitalybaev\GoogleMerchant\Feed;
 
 class GoogleShoppingCommand extends AbstractCommand
 {
@@ -30,15 +32,22 @@ class GoogleShoppingCommand extends AbstractCommand
      */
     private $distributor;
 
+    /**
+     * @var StoreRepositoryInterface
+     */
+    private $storeRepository;
+
     public function __construct(
         DataCollectorInterface $dataCollector,
         ObjectTransformerInterface $objectTransformer,
-        DistributorInterface $distributor
+        DistributorInterface $distributor,
+        StoreRepositoryInterface $storeRepository
     )
     {
         $this->dataCollector = $dataCollector;
         $this->objectTransformer = $objectTransformer;
         $this->distributor = $distributor;
+        $this->storeRepository = $storeRepository;
 
         parent::__construct();
     }
@@ -63,8 +72,17 @@ class GoogleShoppingCommand extends AbstractCommand
         $params['file_url'] = $path;
 
         $result = $this->dataCollector->collect($params);
-        $data = $this->objectTransformer->transform($result, null, $params);
-        $this->distributor->distribute($data, $params);
+
+        $store = $this->storeRepository->find($params['store']);
+        $feed = new Feed($store->getName(), $params['file_url'], null);
+
+        foreach ($result as $item) {
+            $entry = $this->objectTransformer->transform($item, null, $params);
+
+            $feed->addProduct($entry);
+        }
+
+        $this->distributor->distribute($feed, $params);
 
         $output->writeln('Google Shopping feed successfully generated');
         $output->writeln('The feed is stored under: ' . $path);
